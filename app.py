d35f060695ef9dc153feeb657d1e772880782808
U.K <ugne.kilimaite@gmail.com>
2025-07-24 22:20:27 +0300

    Atnaujinta login, sutvarkyta struktūra

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from database import app, db
 from flask_login import LoginManager
 from models.user import User
-from models.admin import Admin
+from flask_migrate import Migrate
 
 login_manager = LoginManager(app)
 login_manager.init_app(app)
@@ -9,10 +9,12 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return db.session.get(User, int(user_id)) or db.session.get(Admin, int(user_id))
+    return db.session.get(User, int(user_id))
+
 
 import services.login
 
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- services/login.py
+++ services/login.py
@@ -1,15 +1,20 @@
 from database import db, app
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_login import LoginManager,login_user, logout_user, login_required, current_user
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
 from sqlalchemy import select, func
 from flask_admin.contrib.sqla import ModelView
 from models.user import User
-from models.admin import Admin
+from forms.login_form import LoginForm
 
 
 class ProtectedModelView(ModelView):
     def is_accessible(self):
-        return current_user.is_authenticated
+        return current_user.is_authenticated and current_user.user_role==1
+    
+    def inaccessible_callback(self, name, **kwargs):
+        flash("Neturite prieigos.", "danger")
+        return redirect(url_for('login'))
+
     
 @app.route('/')
 def index():
@@ -17,51 +22,46 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    form= LoginForm()
+
     if current_user.is_authenticated:
         # Automatiškai nukreipti pagal rolę
-        if isinstance(current_user, Admin):
+        if current_user.user_role ==1:
             return redirect(url_for('admin_dashboard'))
         else:
             return redirect(url_for('user_dashboard'))
-        
-    
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
 
-        stmt = select(Admin).where(Admin.admin_name == username)
-        admin = db.session.execute(stmt).scalar_one_or_none()
-
-        if admin and admin.check_password(password):
-            login_user(admin)
-            return redirect(url_for('admin_dashboard')) # dar nesukurtas html, tai kolkas palieku šitaip
-
-        stmt = select(User).where(User.user_email == username)
+    if form.validate_on_submit():
+        stmt = select(User).where(User.user_email == form.email.data)
         user = db.session.execute(stmt).scalar_one_or_none()
 
-        if user and user.check_password(password):
+        if user and user.check_password(form.password.data):
             login_user(user)
-            return redirect(url_for('user_dashboard')) # dar nesukurtas html, tai kolkas palieku šitaip
+            if user.user_role == 1:
+                return redirect(url_for('admin_dashboard'))
+            else:
+                return redirect(url_for('user_dashboard'))
+        else:
+            flash("Neteisingi prisijungimo duomenys.", "danger")
 
-        flash("Neteisingi prisijungimo duomenys.", "danger")
-
-    return render_template('login.html')
+    return render_template('login.html', form=form)
 
 @app.route('/admin_dashboard')
 @login_required
 def admin_dashboard():
-    if not isinstance(current_user, Admin):
+    if current_user.user_role != 1:
         flash("Neturite prieigos prie administratoriaus puslapio.", "danger")
         return redirect(url_for('login'))
-    return "Sveiki, administratoriau!"
+    return render_template("admin_dashboard.html")
+
 
 @app.route('/user_dashboard')
 @login_required
 def user_dashboard():
-    if not isinstance(current_user, User):
+    if current_user.user_role != 2:
         flash("Neturite prieigos prie naudotojo puslapio.", "danger")
         return redirect(url_for('login'))
-    return "Sveiki, vartotojau!"
+    return "Sveiki prisijungę, turite vartotojo teises"
 
 
 @app.route('/logout')
