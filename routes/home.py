8eb8f2236bb59314d184d4afa7429e0113c166b7
Marko <valmaras@gmail.com>
2025-07-28 21:22:23 +0300

    base.html pakoregavau

--- routes/home.py
+++ routes/home.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, render_template 
 
 home_bp = Blueprint('home', __name__, url_prefix='/', template_folder='../templates')
 
 @home_bp.route('/')
 def index():
-    return redirect(url_for('login.login'))
+    return render_template('index.html')
