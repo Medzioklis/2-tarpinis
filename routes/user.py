10450037ba9281bb7356bef7914c4781c68daff4
U.K <ugne.kilimaite@gmail.com>
2025-07-31 21:01:40 +0300

    Atnaujintas nupirkimas, sukurtas papildomas HTML po nupirkimo

--- routes/user.py
+++ routes/user.py
@@ -4,6 +4,8 @@ from services.user_functions import top_up_balance
 from services.order_services import get_cart_contents, add_product_to_cart, create_order_from_cart
 from services.product_services import get_product_by_id
 from services.order_services import update_cart_item_quantity, remove_item_from_cart
+from models.cart_class import Cart, db
+from sqlalchemy import select
 
 
 user_bp = Blueprint('user', __name__, url_prefix='/user')
@@ -89,6 +91,34 @@ def remove_item(product_id):
     except Exception as e:
         flash(f"Klaida šalinant prekę: {e}", "danger")
     return redirect(url_for('user.cart'))
+
+
+@user_bp.route('/buy_cart_success', methods=['POST'])
+@login_required
+def buy_cart_success():
+    from sqlalchemy import select
+    stmt = select(Cart).where(Cart.user_id == current_user.id) # 1. Išrenkame visas krepšelio prekes naudotojui
+    cart_items = db.session.scalars(stmt).all()
+
+    if not cart_items:# 2. Jei krepšelis tuščias – rodomas pranešimas ir grąžinama atgal į krepšelį
+        flash("Krepšelis tuščias.", "danger")
+        return redirect(url_for("user.cart"))
+    
+    total = sum(item.product.price * item.quantity for item in cart_items) # 3. Apskaičiuojama bendra pirkinių suma
+
+    if current_user.user_balance < total:     # 4. Jei naudotojo balansas per mažas – rodomas pranešimas ir grąžinama atgal
+        flash("Nepakanka lėšų.", "danger")
+        return redirect(url_for("user.cart"))
+  
+    current_user.user_balance -= total # 5. Nuskaitome naudotojo balansą
+
+    for item in cart_items: # 6. Pašaliname visas prekes iš krepšelio (tarsi jas "nupirkome")
+        db.session.delete(item)
+
+    db.session.commit()
+
+    flash("Prekės sėkmingai įsigytos!", "success")
+    return render_template("purchase_success.html", user=current_user, items=cart_items)
 
 
 
